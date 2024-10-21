59697ca6cab65330da57d3f5abfd02957019c9e3
CJ <ogbogu.pmc@gmail.com>
2024-10-21 02:12:47 +0100

    Update sequence.py
    Code Clean Up - tidy the code remove unnecessary lines and restructuring to appeal more presentable.
    
    Buzzer, LED, PIR Pins - have been relocated within the 'Active' function.
    
    Onboard Temp - Onboard temperature sensor - useful for identify how hard  the PICO is working at the current time of the motion detection.
    
    datalog.txt file -A text will record each time the motion sensor is tripped. recording date, time and onboard temperature. With file handling I am apple to append the file adding a new line for each detection made by the PIR sensor. 
    
    Time Stamp - Recording the date and time used to help identify key data when converted into a text file.

--- sequence.py
+++ sequence.py
@@ -1,5 +1,6 @@
 from machine import Pin, PWM, Timer, I2C
 import utime
+import machine
 
 def LEDon(): # Onboard LED - Flash setting
     inled = Pin("LED", Pin.OUT)
@@ -11,44 +12,58 @@ def LEDon(): # Onboard LED - Flash setting
 def LEDoff(): # Onboard LED - Off
     inled = Pin("LED", Pin.OUT)
     inled.value(0)  
+    
+def Active(): # Activating the alarm system
+    # PIR (Motion) Sensor
+    PIR = Pin(0, Pin.IN, Pin.PULL_DOWN)
 
-# PIR (Motion) Sensor
-PIR = Pin(0, Pin.IN, Pin.PULL_DOWN)
+    # RGB LED
+    Led_R = PWM(Pin(4))
+    Led_G = PWM(Pin(3))
+    Led_B = PWM(Pin(2))
+    
+    # Set the frequency to 2KHz
+    Led_R.freq(2000)  
+    Led_G.freq(2000)   
+    Led_B.freq(2000)
 
-# RGB LED
-Led_R = PWM(Pin(4))
-Led_G = PWM(Pin(3))
-Led_B = PWM(Pin(2))
-
-# Buzzer 
-buzzer = PWM(Pin(12, Pin.IN)) #Sound
-
-# Set the frequency to 2KHz
-Led_R.freq(2000)  
-Led_G.freq(2000)   
-Led_B.freq(2000)
-buzzer.duty_u16(1000)
-
-# Activating the alarm system    
-def Active():
+    # Buzzer 
+    buzzer = PWM(Pin(12, Pin.IN)) #Sound
+    buzzer.duty_u16(1000)
+    
+    ## print(PIR.value())
     if PIR.value() == 1:
-        print(PIR.value())
         buzzer.freq(750)
         Led_R.duty_u16(65535)
         Led_G.duty_u16(0)
         Led_B.duty_u16(0)
+        
+        # Time Delay for 3 seconds
         utime.sleep(3)
+        
+        # Onboard Tempreture Sensor
+        sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)
+        conversion_factor = 3.3 / (65535)
+        
+        rtc=machine.RTC()
+        
+        # Appending data log file with each new dection
+        file = open("datalog.txt", "a")
+        reading = sensor_temp.read_u16() * conversion_factor
+        
+        # Timestamp
+        timestamp=rtc.datetime()
+        temperature = 27 - (reading - 0.706)/0.001721
+        timestring="Date: %04d-%02d-%02d    |    Time: %02d:%02d:%02d    |    "%(timestamp[0:3] + timestamp[4:7])
+        file.write(timestring + "State: Motion Detected!    |    Temp: " + str(temperature) + "\n")
+        file.flush() 
     else:
-        print(PIR.value())
         buzzer.deinit()
         Led_R.duty_u16(0)
         Led_G.duty_u16(65535)
         Led_B.duty_u16(0)
         
 def InActive():
-    if PIR.value() == 0:
-        print("Alarm Deactivated")
-        Led_R.duty_u16(0)
-        Led_G.duty_u16(0)
-        Led_B.duty_u16(65535)
-        utime.sleep(300)
+    Led_R.duty_u16(0)
+    Led_G.duty_u16(0)
+    Led_B.duty_u16(65535)
