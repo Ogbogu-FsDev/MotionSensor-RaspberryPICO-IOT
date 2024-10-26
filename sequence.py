4fadce85cbd77190a24661203e4553bddb38ba84
CJ Ogbogu <ogbogu.pmc@gmail.com>
2024-10-26 21:47:17 +0100

    Update sequence.py
    Reducing the amount of code used and correcting declared variables.

--- sequence.py
+++ sequence.py
@@ -2,44 +2,43 @@ from machine import Pin, PWM, Timer, I2C
 import utime
 import machine
 
+inled = Pin("LED", Pin.OUT)
+
+# PIR (Motion) Sensor
+PIR = Pin(0, Pin.IN, Pin.PULL_DOWN)
+
+# RGB LED
+Led_R = PWM(Pin(4))
+Led_G = PWM(Pin(3))
+Led_B = PWM(Pin(2))
+
+# Set the frequency to 2KHz
+Led_R.freq(2000)  
+Led_G.freq(2000)   
+Led_B.freq(2000)
+
+# Buzzer 
+buzzer = PWM(Pin(15, Pin.IN)) #Sound
+
 def LEDon(): # Onboard LED - Flash setting
-    inled = Pin("LED", Pin.OUT)
     utime.sleep_ms(100)
     inled.value(1)
     utime.sleep_ms(100)
     inled.value(0)
-    
-def LEDoff(): # Onboard LED - Off
-    inled = Pin("LED", Pin.OUT)
-    inled.value(0)  
-    
+        
 def Active(): # Activating the alarm system
-    # PIR (Motion) Sensor
-    PIR = Pin(0, Pin.IN, Pin.PULL_DOWN)
-
-    # RGB LED
-    Led_R = PWM(Pin(4))
-    Led_G = PWM(Pin(3))
-    Led_B = PWM(Pin(2))
-    
-    # Set the frequency to 2KHz
-    Led_R.freq(2000)  
-    Led_G.freq(2000)   
-    Led_B.freq(2000)
-
-    # Buzzer 
-    buzzer = PWM(Pin(12, Pin.IN)) #Sound
-    buzzer.duty_u16(1000)
-    
-    ## print(PIR.value())
     if PIR.value() == 1:
-        buzzer.freq(750)
+        buzzer.freq(500)
+        buzzer.duty_u16(1000)
+        
         Led_R.duty_u16(65535)
         Led_G.duty_u16(0)
         Led_B.duty_u16(0)
         
         # Time Delay for 3 seconds
         utime.sleep(3)
+        
+        buzzer.duty_u16(0) # loudness set to 0 = sound off
         
         # Onboard Tempreture Sensor
         sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)
@@ -58,11 +57,10 @@ def Active(): # Activating the alarm system
         file.write(timestring + "State: Motion Detected!    |    Temp: " + str(temperature) + "\n")
         file.flush() 
     else:
-        buzzer.deinit()
         Led_R.duty_u16(0)
         Led_G.duty_u16(65535)
         Led_B.duty_u16(0)
-        
+
 def InActive():
     Led_R.duty_u16(0)
     Led_G.duty_u16(0)
